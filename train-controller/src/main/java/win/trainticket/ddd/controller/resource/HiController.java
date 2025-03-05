1b74d03eec721fec976c991fb112d6a8114c2e59
Thang Nguyen <mrwin95@gmail.com>
2025-03-05 11:40:19 -0700

    first commit ddd project

---
+++ train-controller/src/main/java/win/trainticket/ddd/controller/resource/HiController.java
@@ -0,0 +1,15 @@
+package win.trainticket.ddd.controller.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HiController {
+
+    @GetMapping("/hi")
+    public String hello() {
+        return "Hello controller";
+    }
+}
