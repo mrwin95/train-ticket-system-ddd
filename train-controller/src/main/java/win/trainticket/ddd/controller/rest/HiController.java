658463598267263dec8f2040af435760ade6af8e
Thang Nguyen <mrwin95@gmail.com>
2025-03-06 13:28:31 -0700

    finish test circuit breaker and limiter

---
+++ train-controller/src/main/java/win/trainticket/ddd/controller/rest/HiController.java
@@ -0,0 +1,61 @@
+package win.trainticket.ddd.controller.rest;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import win.trainticket.ddd.application.service.event.EventAppService;
+
+import java.security.SecureRandom;
+
+@RestController
+@RequestMapping("/hello")
+@Slf4j
+public class HiController {
+
+    private final EventAppService eventAppService;
+
+    private static final SecureRandom random = new SecureRandom();
+
+    private final RestTemplate restTemplate;
+
+    public HiController(EventAppService eventAppService, RestTemplate restTemplate) {
+        this.eventAppService = eventAppService;
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/hi")
+    @RateLimiter(name = "backendA", fallbackMethod = "fallBackHello")
+    public String hello() {
+        return eventAppService.sayHi("Win");
+    }
+
+    public String fallBackHello(Throwable throwable) {
+        return "Too Many Calls";
+    }
+
+    @GetMapping("/hi/v1")
+    @RateLimiter(name = "backendB", fallbackMethod = "fallBackHello")
+    public String sayHi() {
+        return eventAppService.sayHi("Rose");
+    }
+
+    @GetMapping("/hi/circuit/breaker")
+    @CircuitBreaker(name = "checkRandom", fallbackMethod = "fallbackCircuitBreaker")
+    public String circuitBreaker() {
+        int productId = random.nextInt(20) + 1;
+
+        String result = restTemplate.getForObject("https://fakestoreapi.com/products/" + productId, String.class);
+        log.info(result);
+        return restTemplate.getForObject("https://fakestoreapi.com/products/" + productId, String.class);
+//        return eventAppService.sayHi("Rose");
+    }
+
+    public String fallbackCircuitBreaker(Throwable throwable) {
+        return "Service fakestoreapi error";// throwable.getMessage();
+    }
+
+}
